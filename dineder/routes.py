0a88088a630c0b7e257910ca3f21c15505aaaba6
karolina jop <jkarolina1@interia.pl>
2023-05-23 16:43:09 +0200

    modified home page

--- dineder/routes.py
+++ dineder/routes.py
@@ -3,7 +3,7 @@ import secrets
 from flask import render_template, url_for, flash, redirect, request, session
 from dineder import app, db, bcrypt, search
 from dineder.forms import RegistrationForm, LoginForm, UpdateAccountForm, ProductForm, PostForm
-from dineder.models import User, Product, CustomerOrder, Post, Ticket, CustomerTicket
+from dineder.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 import stripe
 
@@ -15,6 +15,71 @@ import stripe
 def home():
     # posts = Post.query.all();
     return render_template('home.html')
+
+@app.route("/about")
+def about():
+    # posts = Post.query.all();
+    return render_template('about.html')
+
+# @app.route("/register", methods=['GET', 'POST'])
+# def register():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('home'))
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+#         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+#         db.session.add(user)
+#         db.session.commit()
+#         flash('Your account has been created! You are now able to log in', 'success')
+#         # flash(f'Account created for {form.username.data}!', 'success')
+#         return redirect(url_for('login'))
+#     return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    print(current_user)
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('home'))
+    # form = LoginForm()
+    # if form.validate_on_submit():
+    #     user = User.query.filter_by(email=form.email.data).first()
+    #     if user and bcrypt.check_password_hash(user.password, form.password.data):
+    #         login_user(user, remember=form.remember.data)
+    #         next_page = request.args.get('next')
+    #         return redirect(next_page) if next_page else redirect(url_for('home'))
+    #     else:
+    #         flash('Login Unsuccessful. Please check email and password', 'danger')
+    # return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+# @app.route("/account", methods=['GET', 'POST'])
+# @login_required
+# def account():
+#     customer_id = current_user.id
+#     customer = User.query.filter_by(id=customer_id).first()
+#     orders = CustomerOrder.query.filter_by(customer_id=customer_id).order_by(CustomerOrder.id.desc())
+#     customer_tickets = CustomerTicket.query.filter_by(customer_id=customer_id).order_by(CustomerTicket.id.desc())
+#
+#     form = UpdateAccountForm()
+#     if form.validate_on_submit():
+#         current_user.username = form.username.data
+#         current_user.email = form.email.data
+#         db.session.commit()
+#         flash('Account has been updated!', 'success')
+#         return redirect(url_for('account'))
+#     elif request.method == 'GET':
+#         form.username.data = current_user.username
+#         form.email.data = current_user.email
+#     # image_file = url_for('static', filename='profile_pics/'+current_user.image_file)
+#     # return render_template('account.html', title='Account', image_file=image_file)
+#     return render_template('account.html', title='Account', form=form, customer=customer, orders=orders, customer_tickets=customer_tickets)
+#
 
 
 # @app.route('/payment', methods = ['POST'])
@@ -108,64 +173,64 @@ def home():
 # #     return render_template('offer.html', title='Offer')
 #
 #
-# # @app.route("/register", methods=['GET', 'POST'])
-# # def register():
-# #     if current_user.is_authenticated:
-# #         return redirect(url_for('home'))
-# #     form = RegistrationForm()
-# #     if form.validate_on_submit():
-# #         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-# #         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-# #         db.session.add(user)
-# #         db.session.commit()
-# #         flash('Your account has been created! You are now able to log in', 'success')
-# #         # flash(f'Account created for {form.username.data}!', 'success')
-# #         return redirect(url_for('login'))
-# #     return render_template('register.html', title='Register', form=form)
-#
-#
-# @app.route("/login", methods=['GET', 'POST'])
-# def login():
+# @app.route("/register", methods=['GET', 'POST'])
+# def register():
 #     if current_user.is_authenticated:
 #         return redirect(url_for('home'))
-#     form = LoginForm()
+#     form = RegistrationForm()
 #     if form.validate_on_submit():
-#         user = User.query.filter_by(email=form.email.data).first()
-#         if user and bcrypt.check_password_hash(user.password, form.password.data):
-#             login_user(user, remember=form.remember.data)
-#             next_page = request.args.get('next')
-#             return redirect(next_page) if next_page else redirect(url_for('home'))
-#         else:
-#             flash('Login Unsuccessful. Please check email and password', 'danger')
-#     return render_template('login.html', title='Login', form=form)
-#
-# @app.route("/logout")
-# def logout():
-#     logout_user()
-#     return redirect(url_for('home'))
-#
-# @app.route("/account", methods=['GET', 'POST'])
-# @login_required
-# def account():
-#     customer_id = current_user.id
-#     customer = User.query.filter_by(id=customer_id).first()
-#     orders = CustomerOrder.query.filter_by(customer_id=customer_id).order_by(CustomerOrder.id.desc())
-#     customer_tickets = CustomerTicket.query.filter_by(customer_id=customer_id).order_by(CustomerTicket.id.desc())
-#
-#     form = UpdateAccountForm()
-#     if form.validate_on_submit():
-#         current_user.username = form.username.data
-#         current_user.email = form.email.data
+#         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+#         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+#         db.session.add(user)
 #         db.session.commit()
-#         flash('Account has been updated!', 'success')
-#         return redirect(url_for('account'))
-#     elif request.method == 'GET':
-#         form.username.data = current_user.username
-#         form.email.data = current_user.email
-#     # image_file = url_for('static', filename='profile_pics/'+current_user.image_file)
-#     # return render_template('account.html', title='Account', image_file=image_file)
-#     return render_template('account.html', title='Account', form=form, customer=customer, orders=orders, customer_tickets=customer_tickets)
-#
+#         flash('Your account has been created! You are now able to log in', 'success')
+#         # flash(f'Account created for {form.username.data}!', 'success')
+#         return redirect(url_for('login'))
+#     return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    customer_id = current_user.id
+    customer = User.query.filter_by(id=customer_id).first()
+    orders = CustomerOrder.query.filter_by(customer_id=customer_id).order_by(CustomerOrder.id.desc())
+    customer_tickets = CustomerTicket.query.filter_by(customer_id=customer_id).order_by(CustomerTicket.id.desc())
+
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    # image_file = url_for('static', filename='profile_pics/'+current_user.image_file)
+    # return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', form=form, customer=customer, orders=orders, customer_tickets=customer_tickets)
+
 #
 # @app.route("/offer")
 # def getProduct():
